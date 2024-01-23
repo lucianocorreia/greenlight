6083f8cd721874503f83e8eefeb9890b9c032102
Luciano Correia <luciano.correia@gmail.com>
2024-01-23 23:46:52 +0400

    shutdown the server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -81,20 +78,22 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
+	// srv := &http.Server{
+	// 	Addr:         fmt.Sprintf(":%d", cfg.port),
+	// 	Handler:      app.routes(),
+	// 	ErrorLog:     log.New(logger, "", 0),
+	// 	IdleTimeout:  time.Minute,
+	// 	ReadTimeout:  10 * time.Second,
+	// 	WriteTimeout: 30 * time.Second,
+	// }
 
-	logger.Info("Starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-	err = srv.ListenAndServe()
+	// logger.Info("Starting server", map[string]string{
+	// 	"addr": srv.Addr,
+	// 	"env":  cfg.env,
+	// })
+	// err = srv.ListenAndServe()
+
+	err = app.serve()
 	if err != nil {
 		logger.Fatal(err, nil)
 	}
